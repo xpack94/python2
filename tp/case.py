b35857f0217b2676a8cff29a21a9453721861bc1
abdesselam <void94@live.fr>
2017-03-11 18:05:02 -0500

    printing the board done

--- tp/case.py
+++ tp/case.py
@@ -1,18 +1,24 @@
 
 
 class Case:
-    def __init__(self,number=0,etat="00"):
+    def __init__(self,number=0,etat="00",dernierCoup=0):
         self._number=number
         self._etat=etat
-        self._signe=self.decoder(self._etat)
+        self._signe=self.decoder(self._etat,dernierCoup)
 
 
 
-    def decoder(self,etat):
+    def decoder(self,etat,dernierCoup):
         if etat=="00":
             return "."
-        elif etat=="01":
-            return "x"
-        else:
-            return "o"
+        elif (etat=="01") :
+            if self._number==dernierCoup:
+                return "X"
+            else:
+                return "x"
+        elif etat=="10":
+            if self._number==dernierCoup:
+                return "O"
+            else:
+                return "o"
 
--- tp/ticTacToe.py
+++ tp/ticTacToe.py
@@ -2,30 +2,34 @@ from case import Case
 
 
 class tic_tac_toe:
-    def __init__(self,number=0,etat=""):
-        c=Case(number,"00")
+    #dIntervale = debut d'intervale , fInterval = fin d'intervale
+    #car chaque tic_tac_toe a son propre intervale de nombre
+    #exemple le 1er entre [0-8] 2 eme entre [9-17] etc...
+
+    def __init__(self,dIntervale=0,fInterval=0,etat="",dernierCoup=0):
+        c=Case(dIntervale,"00")
         self._grid=[[c] * 3 for i in range(3)]
-        self._grid= self.initialiser(self._grid,number,etat)
-        self.afficher(self._grid)
+        self._grid= self.initialiser(self._grid,dIntervale,etat,dernierCoup)
+
 
 
     #class has method victory
     #method turn
     #method afficher
 
-    def initialiser(self,grid,number,etat):
-        #appeler la method decoder pour le decodage des etats
+    def initialiser(self,grid,number,etat,dernierCoup):
+
         #etat=18bits
         index_low=0
         index_high=2
 
-        c= Case(number,etat[index_low:index_high])
+        c= Case(number,etat[index_low:index_high],dernierCoup)
         for i in range(3):
             for j in range(3):
                grid[i][j]=c
                index_low=index_high
                index_high+=2
-               c=Case(c._number+1,etat[index_low:index_high])
+               c=Case(c._number+1,etat[index_low:index_high],dernierCoup)
 
 
 
@@ -35,23 +39,21 @@ class tic_tac_toe:
 
 
 
-
-
-
-
-
-    def afficher(self,grid):
-        for i in range(3):
-            for j in range(3):
-                print(grid[i][j]._signe ,end="")
-                print(" ",end="")
-            print()
-
-
+    # def affiche(self):
+    #     e=""
+    #     for i in range(3):
+    #         for j in range(3):
+    #            e+= str(self._grid[i][j]._signe )
+    #            e+=str(" ")
+    #         e+=str("| ")
+    #
+    #
+    #
+    #     return e
 
 
 def main():
 
-    t=tic_tac_toe(0,"010000111000000000")
+    t=tic_tac_toe(0,8,"100100100100100010")
+    print(t.affiche())
 
-main()
