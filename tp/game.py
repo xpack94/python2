c31643ceaef3c559eb3f310ed5e0e9b63ff2ec60
abdesselam <void94@live.fr>
2017-03-14 00:16:26 -0400

    Revert "jouer un coup , ajoutée"
    This reverts commit dcfdedd1183afb759a7c5d58a99dae031c4582a5.

--- tp/game.py
+++ tp/game.py
@@ -1,6 +1,5 @@
-from case import Case
 from ticTacToe import tic_tac_toe
-import random
+
 
 class theGame:
     #le constructeur prend un seul parametre qui correspond a la configuration d'entier(decimale)
@@ -13,109 +12,31 @@ class theGame:
 
 
     # cette methode initialise le la grille du UTTT
-    #t= top et correspond a la 1er ligne de la grille du jeu
-    #m=middle et correpond a la 2eme ligne de la grille du jeu
-    #b=bottom et correspond a la 3eme ligne de la grille du jeu
     def initialiser(self,jeu, config,dernierCoup):
         c = config[7:]
         low_index=0
         high_index=18
         debut=0
         fin=8
-
+        t = tic_tac_toe(debut, fin, c[low_index:high_index],dernierCoup)
         for i in range(3):
             for j in range(3):
-                if i==0:
-                    nom="t"+str(j+1)
-                elif i==1:
-                    nom="m"+str(j+1)
-                else:
-                    nom="b"+str(j+1)
 
-                t = tic_tac_toe(debut, fin, c[low_index:high_index], dernierCoup,nom)
                 jeu[i][j]=t
                 debut=fin+1
                 fin+=9
                 low_index=high_index
                 high_index+=18
-
-
-
+                t=tic_tac_toe(debut,fin,c[low_index:high_index],dernierCoup)
 
         return jeu
 
 
 
 
-    def jouerCoup(self,jeu,configuration,dernierCoup):
-        dCoup=int(dernierCoup,2)
-        #le caractere qui represente le dernier coup joué est placé dans la variable tour
-        case_a_jouer,tour=self.chercher_nom_de_la_cas_du_dernierCoup(dCoup,jeu)
-        if case_a_jouer!="":
-            for i in range(3):
-                for j in range(3):
-                    if jeu[i][j]._nom==case_a_jouer:
-                        coups_possible=self.trouver_coups_possibles(jeu[i][j]._grid)
-                        #genérer un randome entre 0 et la taille du tableau coups_possible
-                        #la variable coup est affectée le numero du coup a jouer
-                        coup=coups_possible[random.randint(0,len(coups_possible)-1)]
-                        self.jouer(jeu[i][j],coup,tour,case_a_jouer)
 
 
-    #methode qui traverse la grille du jeu principale et renvoi le nom de la case du dernier coup joué
-    def chercher_nom_de_la_cas_du_dernierCoup(self,dernierCoup,jeu):
-        for i in range(3):
-            for j in range(3):
-                if jeu[i][j]._dIntervale<= dernierCoup <=jeu[i][j]._fIntervale :
-                    return self.trouver_nom_de_la_case(jeu[i][j]._grid,dernierCoup)
-
-
-        return "" ,"00"
-
-
-
-    #mthode qui traverse une grille de tic_tac_toe ou le dernier coup a etait joué et renvoi son nom
-    #elle permet de trouver ou le prochain coup sera jouer
-    def trouver_nom_de_la_case(self,grid,dernierCoup):
-        for i in range(3):
-            for j in range(3):
-                if grid[i][j]._number ==dernierCoup:
-                    #retourner le nom de la case du dernier coup et retourner l'etat du dernier coup
-                    #pour savoir a quel tour c'est
-                    return grid[i][j]._nom , grid[i][j]._etat
-
-
-
-        return ""
-
-
-    #methode qui cherche tout les coups possible a jouer et les mets dans un tableau
-    def trouver_coups_possibles(self,jeu):
-        coups_possible=[]
-        for i in range(3):
-            for j in range(3):
-                if jeu[i][j]._etat=="00":
-                    coups_possible.append(jeu[i][j]._number)
-
-
-
-        return coups_possible
-
-
-    def jouer(self,jeu,coup,tour,nom):
-        if tour=="01":
-            tour="10"
-        else:
-            tour="01"
-        for i in range(3):
-            for j in range(3):
-                if jeu._grid[i][j]._number == coup:
-                     c=Case(coup,tour,nom,coup)
-                     jeu._grid[i][j]=c
-
-
-
-    #la methode display permet d'afficher la grille du  jeu au complet
+    #la methode display permet d'afficher le jeu au complet
     def display(self):
         self.printLine(0)
         print("-------------------------------")
@@ -156,12 +77,7 @@ class theGame:
 
 def main():
     t=theGame("459329034283597291728327479273734123420780266358036")
-    #t.display()
-    t.jouerCoup(t._jeu,"459329034283597291728327479273734123420780266358036",t._dernierCoup)
     t.display()
-
-
-
 
 
 
--- tp/ticTacToe.py
+++ tp/ticTacToe.py
@@ -6,13 +6,10 @@ class tic_tac_toe:
     #car chaque tic_tac_toe a son propre intervale de nombre
     #exemple le 1er entre [0-8] 2 eme entre [9-17] etc...
 
-    def __init__(self,dIntervale=0,fIntervale=0,etat="",dernierCoup=0,nom=""):
+    def __init__(self,dIntervale=0,fInterval=0,etat="",dernierCoup=0):
         c=Case(dIntervale,"00")
         self._grid=[[c] * 3 for i in range(3)]
-        self._nom=nom
         self._grid= self.initialiser(self._grid,dIntervale,etat,dernierCoup)
-        self._dIntervale=dIntervale
-        self._fIntervale=fIntervale
 
 
 
@@ -26,23 +23,13 @@ class tic_tac_toe:
         index_low=0
         index_high=2
 
-
+        c= Case(number,etat[index_low:index_high],dernierCoup)
         for i in range(3):
             for j in range(3):
-                if i==0:
-                    nom="t"+str(j+1)
-                elif i==1:
-                    nom="m"+str(j+1)
-                else:
-                     nom="b"+str(j+1)
-
-
-                c = Case(number, etat[index_low:index_high],nom,dernierCoup)
-                grid[i][j]=c
-                index_low=index_high
-                index_high+=2
-                number=c._number+1
-
+               grid[i][j]=c
+               index_low=index_high
+               index_high+=2
+               c=Case(c._number+1,etat[index_low:index_high],dernierCoup)
 
 
 
@@ -52,9 +39,21 @@ class tic_tac_toe:
 
 
 
+    # def affiche(self):
+    #     e=""
+    #     for i in range(3):
+    #         for j in range(3):
+    #            e+= str(self._grid[i][j]._signe )
+    #            e+=str(" ")
+    #         e+=str("| ")
+    #
+    #
+    #
+    #     return e
+
+
 def main():
 
     t=tic_tac_toe(0,8,"100100100100100010")
-
-
+    print(t.affiche())
 
